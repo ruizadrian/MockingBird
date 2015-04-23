5da8b39882679aa804cc8f2e6d61aa35ab42e91c
atsullivan <atsullivan23@gmail.com>
2015-04-23 12:09:08 -0700

    added mockingbird.js
    still need generator

---
+++ MockingBird.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var argv = require('optimist')
+  .usage('$0 [-t] [-a] [-o] [-i] [--target x86|c|js] filename')
+  .boolean(['t','a','o','i'])
+  .describe('t', 'dump tokens after scanning then stop')
+  .describe('a', 'dump abstract syntax tree after parsing then stop')
+  .describe('o', 'do optimizations')
+  .describe('i', 'generate and show the intermediate code then stop')
+  .describe('target', 'generate code for target language: x86, C, or JavaScript')
+  .default({target:'js'})
+  .check(function (argv) {return /^(x86|c|js)$/.test(argv.target)})
+  .demand(1)
+  .argv
+
+var scan = require('./scanner')
+var parse = require('./parser')
+var generate = require('./generators/' + argv.target + 'generator')
+
+scan(argv._[0], function (tokens) {
+  if (argv.t) {
+    tokens.forEach(function (t) {console.log(t)})
+    return
+  }
+  var program = parse(tokens)
+  if (argv.a) {
+    console.log(program.toString())
+    return
+  }
+  if (argv.o) {
+    program = program.optimize()
+  }
+  program.analyze()
+  if (argv.i) {
+    program.showSemanticGraph()
+    return
+  }
+  generate(program)
+})
